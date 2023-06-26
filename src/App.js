d6640cbe5a4b047cc20ef97f968bdcd5ce2fa730
Tia4130 <Tiaakiki12@gmail.com>
2023-06-26 17:19:01 +0300

    adding picture and wallpaper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+//IMPORT IMAGES
+import tibouPerfume from './image/logo3.png';
+import tiboulb from './image/logo33-100.png'
 function App() {
   return (
     <div className="App">
-      hi
+      <div className='container'>
+        <div className='tiboulb'>
+          <img src={tiboulb} alt="tibou.lb" />
+        </div>
+
+        <div className='tibouperfume'>
+          <img src={tibouPerfume} alt="tibou perfume" />
+        </div>
+      </div>
     </div>
   );
 }
